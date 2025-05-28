a053032d907f2325bda7ec8ba1c9b368e42ee34b
Prashant20nov2003 <prashanttbhardwajj@gmail.com>
2025-05-29 03:47:35 +0530

    removed api key from the code

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,15 +3,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDegmON3eLvy5bOvMLcWeFDRhOkXmfQ3gg",
-  authDomain: "food-ordering-app-2a15b.firebaseapp.com",
-  projectId: "food-ordering-app-2a15b",
-  storageBucket: "food-ordering-app-2a15b.firebasestorage.app",
-  messagingSenderId: "679998251687",
-  appId: "1:679998251687:web:0690a71e9a795b1bf93182",
-  measurementId: "G-6Z43ZQZ1E0"
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id",
+  measurementId: "your-measurement-id"
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
